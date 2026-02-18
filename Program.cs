d9dda9780b7876fa6f4741b4831874178f5c7ff6
Mohammed-cell-gif <moal08008@utb.vaxjo.se>
2026-02-18 18:34:13 +0100

    Programmeringklasser-del1-del1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Fordon
+{
+    private string tillverkare;
+    private string modell;
+    private int arsmodell;
+    private double vikt;
+
+    public void Settillverkare(string tv)
+    {
+        tillverkare=tv;
+    }
+    public string Gettillverkare(){return tillverkare;}
+    public void Setmodell(string m) {modell=m;}
+    public string Getmodell() {return modell;}
+    public void Setarsmodell(int am){arsmodell=am;}
+    public int Getarsmodell(){ return arsmodell;}
+    public void Setvikt(double v){vikt=v;}
+    public double Getvikt(){return vikt;}
+}
